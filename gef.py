d7805a3b5f0567526653b8af70e6f877bb3ac430
psiquo <33759674+psiquo@users.noreply.github.com>
2022-10-24 03:16:12 +0200

    Fixed empty file when using shellcode get

--- gef.py
+++ gef.py
@@ -6772,7 +6772,7 @@ class ShellcodeGetCommand(GenericCommand):
     _aliases_ = ["sc-get",]
 
     api_base = "http://shell-storm.org"
-    get_url = f"{api_base}/shellcode/files/shellcode-{{:d}}.php"
+    get_url = f"{api_base}/shellcode/files/shellcode-{{:d}}.html"
 
     def do_invoke(self, argv: List[str]) -> None:
         if len(argv) != 1:
